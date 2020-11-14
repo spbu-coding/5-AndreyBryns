bab7702195060d219bc52e248b7f71218de1d4a1
unknown <abryns3d@gmail.com>
2020-11-14 23:59:14 +0300

    upd

--- src/main.c
+++ src/main.c
@@ -10,11 +10,14 @@ void input()
 {
 	char buffer[20];
 	scanf("%s", buffer);
+	fflush(stdout);
 	printf("%s", buffer);
 }
 
 int main()
 {
+	printf("&p\n", &other);
+	fflush(stdout);
 	input();
 	return 0;
 }
